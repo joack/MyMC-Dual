7e362e2e14b1337da4d995163c36059a092ba6fb
joack <https://joack@github.com/joack>
2017-05-05 01:21:52 -0300

    Project Update

---
+++ MyMC/Preference.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Joack
+ * Fecha: 05/05/2017
+ * Hora: 00:44
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyMC
+{
+	/// <summary>
+	/// Description of Preference.
+	/// </summary>
+	public partial class Preferences : Form
+	{
+		public Preferences()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
